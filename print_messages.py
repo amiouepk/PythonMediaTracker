4e3745514920ea0bf866c421d49e1c558a6f5900
amiouepk <anish1mani@gmail.com>
2026-01-10 01:43:53 -0500

    2nd commit

---
+++ print_messages.py
@@ -0,0 +1,30 @@
+
+def inputLine(str_input):
+    print(f"Enter {str_input}\n")
+
+def helpMessage():
+    print(f"Help message\n")
+
+def startMessage():
+
+
+    print(f"-----------------Welcome-----------------")
+    print(f"Options:\n")
+    print(f"1. \n")
+    print(f"2. \n")
+    print(f"3. Exit\n")
+    print(f"4. \n")
+
+    match str_input == inputLine(str_input)
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    print("huh")
